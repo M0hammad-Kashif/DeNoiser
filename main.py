e3b8c7ae0f85713ec733b979b2c8209de392558b
M0hammad-Kashif <iamkashif20@gmail.com>
2022-07-09 22:19:55 +0530

    Updated paths in main.py

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ st.subheader("")
 st.subheader("Input Speech Sample")
 
 if file_uploader is not None:
-    with open(os.path.join("./savedFiles", file_uploader.name), "wb") as f:
+    with open(os.path.join(file_uploader.name), "wb") as f:
         f.write(file_uploader.getbuffer())
     handle_uploaded_audio_file(file_uploader)
     st.subheader("")
     st.subheader("")
 
-    out = predict(os.path.join("./savedFiles", file_uploader.name))
+    out = predict(os.path.join(file_uploader.name))
 
     wav_encoder = tf.audio.encode_wav(out, 16000)
-    wav_saver = io_ops.write_file('./outputFiles/output.wav', wav_encoder)
+    wav_saver = io_ops.write_file('output.wav', wav_encoder)
 
-    audio_file = open('./outputFiles/output.wav', 'rb')
+    audio_file = open('output.wav', 'rb')
     audio_bytes = audio_file.read()
 
     st.subheader("Output Speech Sample")
