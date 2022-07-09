92b1e18e4ef73c77536f9fe20882ceee3ffb0866
M0hammad-Kashif <iamkashif20@gmail.com>
2022-07-09 22:09:08 +0530

    Updated utils

--- utils.py
+++ utils.py
@@ -41,21 +41,3 @@ def predict(path):
     final_op = tf.reshape(predictions[:-1], ((predictions.shape[0] - 1) * predictions.shape[1], 1))
     final_op = tf.concat((final_op, predictions[-1][-diff:]), axis=0)
     return final_op
-
-
-# def predict_tflite(path):
-#     test_audio, diff = inference_preprocess(path)
-#     input_index = interpreter.get_input_details()[0]["index"]
-#     output_index = interpreter.get_output_details()[0]["index"]
-
-#     preds = []
-#     for i in test_audio:
-#         interpreter.set_tensor(input_index, tf.expand_dims(i, 0))
-#         interpreter.invoke()
-#         predictions = interpreter.get_tensor(output_index)
-#         preds.append(predictions)
-
-#     predictions = tf.squeeze(tf.stack(preds, axis=1))
-#     final_op = tf.reshape(predictions[:-1], ((predictions.shape[0] - 1) * predictions.shape[1], 1))
-#     final_op = tf.concat((tf.squeeze(final_op), predictions[-1][-diff:]), axis=0)
-#     return final_op
