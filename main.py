d91251ae2524a644ebd4fe1f68fb5cbe4a08bac9
haiderzm <haider1272002@gmail.com>
2022-07-10 12:28:12 +0530

    Implementation without saving audio

--- main.py
+++ main.py
@@ -12,21 +12,44 @@ file_uploader = st.sidebar.file_uploader(label="", type=".wav")
 st.subheader("")
 st.subheader("Input Speech Sample")
 
-if file_uploader is not None:
-    with open(file_uploader.name, "wb") as f:
-        f.write(file_uploader.getbuffer())
-    handle_uploaded_audio_file(file_uploader)
-    st.subheader("")
-    st.subheader("")
+def _bytes_feature(value):                                                      
+  """Returns a bytes_list from a string / byte."""                              
+  if isinstance(value, type(tf.constant(0))):                                   
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value])) 
 
-    out = predict(os.path.join(file_uploader.name))
+if file_uploader is not None:
+    # with open(file_uploader.name, "wb") as f:
+    #     f.write(file_uploader.getbuffer())
+    #     st.write(type(file_uploader.read()))
+    #     st.audio((file_uploader.read()))
+    # handle_uploaded_audio_file(file_uploader)
+
+    out = predict(file_uploader)
+
+    st.subheader("")
+    st.subheader("")
+    # st.write(type(out))
 
     wav_encoder = tf.audio.encode_wav(out, 16000)
-    wav_saver = io_ops.write_file('output.wav', wav_encoder)
 
-    audio_file = open('output.wav', 'rb')
-    audio_bytes = audio_file.read()
+    
+    test = wav_encoder.numpy()
+
+    # st.write("Test : ",type(test))
 
     st.subheader("Output Speech Sample")
 
-    st.audio(audio_bytes, format='audio/wav')
+    st.audio(test)
+
+    # st.audio(test)
+
+    # st.audio(wav_encoder
+    # st.write(type(wav_encoder))
+    # wav_saver = io_ops.write_file('output.wav', wav_encoder)
+
+    # audio_file = open('output.wav', 'rb')
+    # audio_bytes = audio_file.read()
+
+
+    # st.audio(audio_bytes, format='audio/wav')
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pydub
 import streamlit as st
 import tensorflow as tf
+import numpy as np
 
 from main import model
 
@@ -9,7 +10,22 @@ batching_size = 12000
 
 def handle_uploaded_audio_file(uploaded_file):
     a = pydub.AudioSegment.from_wav(uploaded_file)
+
+    # st.write("A : ", type(a))
+
+    # st.write("Width : ",a.sample_width)
+
+    samples = a.get_array_of_samples()
+    fp_arr = np.array(samples).T.astype(np.float32)
+    fp_arr /= np.iinfo(samples.typecode).max
+    fp_arr = fp_arr.reshape(fp_arr.shape[0],1)
+    fp_arr = tf.convert_to_tensor(fp_arr, dtype=tf.float32)
+    # st.write(type(fp_arr))
+    # st.write(fp_arr.shape)
+
     st.write(a)
+
+    return fp_arr
 
 
 def audio_to_display(audio):
@@ -20,11 +36,15 @@ def audio_to_display(audio):
 
 def get_audio(path):
     audio, _ = tf.audio.decode_wav(tf.io.read_file(path), 1)
+    # st.write(type(audio))
+    
+    # st.write("Tf Shape : ", audio.shape)
     return audio
 
 
-def inference_preprocess(path):
-    audio = get_audio(path)
+def inference_preprocess(uploaded_file):
+    # audio = get_audio(path)
+    audio = handle_uploaded_audio_file(uploaded_file)
     audio_len = audio.shape[0]
     batches = []
     for i in range(0, audio_len - batching_size, batching_size):
@@ -35,8 +55,8 @@ def inference_preprocess(path):
     return tf.stack(batches), diff
 
 
-def predict(path):
-    test_data, diff = inference_preprocess(path)
+def predict(uploaded_file):
+    test_data, diff = inference_preprocess(uploaded_file)
     predictions = model.predict(test_data)
     final_op = tf.reshape(predictions[:-1], ((predictions.shape[0] - 1) * predictions.shape[1], 1))
     final_op = tf.concat((final_op, predictions[-1][-diff:]), axis=0)
